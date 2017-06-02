ea2c1264567744c970ab2471ac00393e4fc01c8e
Chris <crohr00@gmail.com>
2017-06-02 02:30:04 -0700

    Initial Json parser

---
+++ SFJson/Serializer.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Reflection;
+using System.Text;
+
+namespace SFJson
+{
+    public class Serializer
+    {
+        public object ObjectToSerialize { get; set; }
+
+        public Serializer()
+        {
+
+        }
+
+        public Serializer(object objectToSerialize)
+        {
+            ObjectToSerialize = objectToSerialize;
+        }
+
+        public override string ToString()
+        {
+            return Serialize();
+        }
+
+        public string Serialize()
+        {
+            var sb = new StringBuilder();
+            SerializeObject(sb, ObjectToSerialize);
+            return sb.ToString();
+        }
+
+        private void SerializeObject(StringBuilder sb, object obj)
+        {
+            if(obj == null)
+            {
+                sb.Append("null");
+            }
+            else
+            {
+                sb.Append("{");
+                var typeStringSplit = obj.GetType().AssemblyQualifiedName.Split(',');
+//                Console.WriteLine("Here: " + typeStringSplit);
+                sb.AppendFormat("\"$Type\":\"{0}\"", typeStringSplit[0] + "," + typeStringSplit[1]);
+                SerializeProperties(sb, obj);
+                sb.Append("}");
+            }
+        }
+
+        private void SerializeProperties(StringBuilder sb, object obj)
+        {
+            var properties = obj.GetType().GetProperties(BindingFlags.Instance | BindingFlags.Public);
+            foreach(var propertyInfo in properties)
+            {
+                SerializeProperty(sb, propertyInfo, obj);
+            }
+        }
+
+        private void SerializeProperty(StringBuilder sb, PropertyInfo propertyInfo, object obj)
+        {
+            if(propertyInfo.CanWrite && propertyInfo.CanRead)
+            {
+                if(propertyInfo.PropertyType.IsPrimitive)
+                {
+                    sb.AppendFormat(",\"{0}\":{1}", propertyInfo.Name, propertyInfo.GetValue(obj));
+                }
+                else if(propertyInfo.PropertyType.IsArray)
+                {
+                    // TODO
+                    sb.AppendFormat(",\"{0}\":{1}", propertyInfo.Name, propertyInfo.GetValue(obj));
+                }
+                else
+                {
+                    sb.AppendFormat(",\"{0}\":", propertyInfo.Name);
+                    SerializeObject(sb, propertyInfo.GetValue(obj));
+                }
+            }
+        }
+    }
+}
---
+++ SFJsonTest/SerializerTest.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace SFJsonTest
+{
+    public class SimpleTestObject 
+    {
+    }
+    
+    public class SelfReferencedSimpleObject
+    {
+        public SelfReferencedSimpleObject Inner { get; set; }
+    }
+    
+    public class SimpleTestObjectWithProperties
+    {
+        public int TestInt { get; set; }
+    }
+
+    [TestFixture]
+    public class SerializerTest
+    {
+        [Test] 
+        public void CanSerializeSimpleObjectType() 
+        {
+            var serializer = new SFJson.Serializer();
+            serializer.ObjectToSerialize = new SimpleTestObject();
+            var str = serializer.Serialize();
+
+            Console.WriteLine(str);
+            Assert.AreEqual("{\"$Type\":\"SFJsonTest.SimpleTestObject, SFJsonTest\"}", str);
+        }
+        
+        [Test] 
+        public void CanSerializeSelfReferencedSimpleObjectType() 
+        {
+            var serializer = new SFJson.Serializer();
+            var obj = new SelfReferencedSimpleObject();
+            obj.Inner = new SelfReferencedSimpleObject();
+            serializer.ObjectToSerialize = obj;
+            var str = serializer.Serialize();
+
+            Console.WriteLine(str);
+            Assert.AreEqual("{\"$Type\":\"SFJsonTest.SelfReferencedSimpleObject, SFJsonTest\",\"Inner\":{\"$Type\":\"SFJsonTest.SelfReferencedSimpleObject, SFJsonTest\",\"Inner\":null}}", str);
+        }
+
+        [Test]
+        public void CanSerializeSimpleObjectWithProperties()
+        {
+            var serializer = new SFJson.Serializer();
+            serializer.ObjectToSerialize = new SimpleTestObjectWithProperties
+            {
+                TestInt = 10
+            };
+            var str = serializer.Serialize();
+
+            Console.WriteLine(str);
+            Assert.AreEqual("{\"$Type\":\"SFJsonTest.SimpleTestObjectWithProperties, SFJsonTest\",\"TestInt\":10}", str);
+        }
+    }
+}
